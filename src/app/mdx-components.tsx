614215fdc116cfb6c235965de6d65ee5ca2f868a
lucykim05 <effelt0522@naver.com>
2025-08-14 18:36:53 +0900

    fix: fix to deploy

--- src/app/mdx-components.tsx
+++ src/app/mdx-components.tsx
@@ -1,8 +1,19 @@
-import BlogCalendarInline from '@/components/features/BlogCalendarInline';
+// src/app/mdx-components.tsx
+import type { MDXComponents } from 'mdx/types';
+import BlogCalendarInline, {
+  type InlineEvent,
+} from '@/components/features/BlogCalendarInline';
 
-export function useMDXComponents(components: any) {
+/**
+ * 전역 MDX 컴포넌트 매핑.
+ * - MDX에서 <Calendar />를 쓰면 여기로 바인딩됨.
+ * - props 타입도 명시해서 any 사용 안 함.
+ */
+export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    Calendar: (props: any) => <BlogCalendarInline {...props} />,
+    Calendar: (props: { events?: InlineEvent[] }) => (
+      <BlogCalendarInline events={props.events ?? []} />
+    ),
     ...components,
   };
 }
